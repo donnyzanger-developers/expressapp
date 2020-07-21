366e5373ee49b63c96e0e4b2fc2c78ded1bcf434
James Schiller <schillerj78@gmail.com>
2020-07-21 04:53:24 -0400

    build.sh instructions

--- build.sh
+++ build.sh
@@ -8,6 +8,10 @@
 # configure mongo on ubuntu for docker
 # /etc/mongod.conf on ubuntu /usr/local/etc/mongod.conf on macos
 # change the bindIp from 127.0.0.1 to 0.0.0.0
+# in prod .env set 
+# MONGO_HOST=mongodb://x.x.x.x:27017/<dbname> // where x.x.x.x is internal ip address not external ip address
+# in dev it is
+# MONGO_HOST=mongodb://host.docker.internal:27017/<dbname> // macos docker install uses host.docker.internal
 
 sudo docker build . --tag back2
 sudo docker run --publish 3000:3000 --detach back2
@@ -21,4 +25,5 @@ sudo docker run --publish 3000:3000 --detach back2
 
 # TROUBLESHOOTING
 # sudo docker logs bf7838d8ac04
-
+# or..
+# sudo docker run --rm -it back2
